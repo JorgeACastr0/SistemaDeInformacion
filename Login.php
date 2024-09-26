18c6ca00636af86dea3791e6e80366f9a0fd55ef
Brayhan  Ortiz <brayhanortiz27@gmail.com>
2024-09-26 13:49:34 -0500

    cambio a php

--- Login.php
+++ Login.php
@@ -1,3 +1,10 @@
+<?php
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +13,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS v5.2.1 -->
     <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="CSS/styleLogin.css">
+    
+    <link rel="stylesheet" type="text/CSS" href="CSS/styleLogin.css">
+
 
 
     <title>Login</title>
